78c8e1cd1846f91dd27b85557bb9f797373d2730
lingfeng <lingfeng@douban.com>
2016-10-28 14:45:17 +0800

    fix

--- index.js
+++ index.js
@@ -45,17 +45,17 @@ parser.addArgument(
     }
 );
 parser.addArgument(
-    ['-i', '--increase'], {
+    ['-i', '--inject'], {
         action: 'append',
         type: 'string',
-        help: 'increase field'
+        help: 'inject field'
     }
 );
 parser.addArgument(
-    ['-r', '--reduce'], {
+    ['-d', '--delete'], {
         action: 'append',
         type: 'string',
-        help: 'reduce field'
+        help: 'delete field'
     }
 );
 
@@ -88,18 +88,17 @@ var record_data = function(req, serverResData) {
     fs.appendFile(respFile, beautify(unescape(serverResData.toString().replace(/\\u/g, '%u')), {
         indent_size: 4
     }), function(err, data) {
-        // fs.appendFile(respFile,beautify(serverResData.toString('utf-8'),{indent_size:4}),function (err, data) {
         if (err) {
             return console.error(err);
         }
     });
 };
 
-var gen_mock = function(mockResData, mock_fields, increase_fields, reduce_fields) {
+var gen_mock = function(mockResData, mock_fields, inject_fields, delete_fields) {
     mockResData = JSON.parse(mockResData.toString('utf-8'))
     console.log(mock_fields)
-    console.log(increase_fields)
-    console.log(reduce_fields)
+    console.log(inject_fields)
+    console.log(delete_fields)
     console.log('--------------原始值-----------------')
     console.log(mockResData)
     for (var mock_field of mock_fields) {
@@ -110,15 +109,15 @@ var gen_mock = function(mockResData, mock_fields, increase_fields, reduce_fields
             return mock_value;
         });
     }
-    for (var increase_field of increase_fields) {
-        var seperate_index = increase_field.indexOf('='),
-            key = increase_field.substring(0, seperate_index),
-            mock_value = increase_field.substring(seperate_index + 1);
+    for (var inject_field of inject_fields) {
+        var seperate_index = inject_field.indexOf('='),
+            key = inject_field.substring(0, seperate_index),
+            mock_value = inject_field.substring(seperate_index + 1);
         jp.value(mockResData, key, mock_value)
     }
 
-    for (var reduce_field of reduce_fields) {
-        var paths = jp.paths(mockResData, reduce_field)
+    for (var delete_field of delete_fields) {
+        var paths = jp.paths(mockResData, delete_field)
         for (var path of paths) {
             path.shift()
             lo.unset(mockResData, path)
@@ -144,15 +143,16 @@ var fuzzy_rule = {
         if (/json/i.test(res.headers['content-type'])) {
             var mockResData = serverResData,
                 mock_fields = args.mock ? args.mock : [],
-                increase_fields = args.increase ? args.increase : [],
-                reduce_fields = args.reduce ? args.reduce : [];
+                inject_fields = args.inject ? args.inject : [],
+                delete_fields = args.delete ? args.delete : [];
 
             if (args.api === null) {
                 // callback(serverResData)
                 console.log('指定了规则，但是没有指定api，暴力mock')
                 if (serverResData.toString() != '') {
                     try {
-                        mockResData = gen_mock(mockResData, mock_fields, increase_fields, reduce_fields)
+                        record_data(req, serverResData)
+                        mockResData = gen_mock(mockResData, mock_fields, inject_fields, delete_fields)
                         callback(JSON.stringify(mockResData))
                     } catch (e) {
                         console.log('mock出现错误，返回原始数据')
@@ -163,6 +163,7 @@ var fuzzy_rule = {
                 console.log('指定了规则，也指定了api')
                 var find_flag = false
                 for (var api of args.api) {
+                    console.log(req.url);
                     if (req.url.indexOf(api) != -1) {
                         find_flag = true
                         break
@@ -172,7 +173,7 @@ var fuzzy_rule = {
                     try {
                         record_data(req, serverResData)
                         console.log('指定的api命中！！！ 返回mock数据')
-                        mockResData = gen_mock(mockResData, mock_fields, increase_fields, reduce_fields)
+                        mockResData = gen_mock(mockResData, mock_fields, inject_fields, delete_fields)
                         callback(JSON.stringify(mockResData))
                     } catch (e) {
                         console.log('mock出现错误，返回原始数据')
@@ -191,11 +192,11 @@ var fuzzy_rule = {
 
 var options = {
     rule: fuzzy_rule,
-    disableWebInterface: true,
+    // disableWebInterface: true,
     port: args.port
         // silent: true
 }
-if (args.mock === null && args.increase === null && args.reduce === null) {
+if (args.mock === null && args.inject === null && args.delete === null) {
     delete options.rule
     console.log('没有指定规则，返回原始response...')
 }
